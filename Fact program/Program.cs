e91d955bd9cdc52cbb13aac1cd919ad035fe170c
indhumathi <Indhumathi.Ramesh@disys.com>
2022-02-11 06:46:06 -0800

    Add project files.

---
+++ Fact program/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Factorial
+{
+    public static void Main(string[] args)
+    {
+        int value, fact = 1, number;
+        Console.Write("Enter any Number: ");
+        number = int.Parse(Console.ReadLine());
+        for (value = 1; value <= number; value++)
+        {
+            fact = fact * value;
+        }
+        Console.Write("Factorial of " + number + " is: " + fact);
+    }
+}
